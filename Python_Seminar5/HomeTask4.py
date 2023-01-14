41deca682abf9f9161ec52d451df70b07b7fd00c
Alexey Parshin <jax77@mail.ru>
2023-01-14 15:24:57 +0300

    Добавил входной и выходной текстовый файл к заданию 4 (семинар 5)

--- Python_Seminar5/HomeTask4.py
+++ Python_Seminar5/HomeTask4.py
@@ -2,9 +2,14 @@
 # Алгоритм RLE (англ. Run-Length Encoding) — алгоритм сжатия, заменяющий ИДУЩИЕ ПОДРЯД одинаковые символы парой (повторяющийся символ, количество повторений).
 # Например, строчку aaababbcbbb он переводит в (a, 3) (b, 1) (a, 1) (b, 2) (c, 1) (b, 3).
 
+with open('HomeTask4_start.txt', 'r', encoding='utf-8') as file:
+    spisok = file.read()
+    file.close()
 
-print('Введите строку английских букв:')
-spisok = list(input())
+# print('Введите строку английских букв:')
+# spisok = list(input())
+
+print('Исходная строка английский букв:')
 print(spisok)
 
 count = 1
@@ -24,4 +29,12 @@ for i in range(1, len(spisok)):
         character_list.append(spisok[i])
 
 print('Результат RLE алгоритма:')
-print(list(zip(character_list, count_list)))
+
+final = list(zip(character_list, count_list))
+
+print(final)
+
+with open('HomeTask4_finish.txt', 'w', encoding='utf-8') as file:
+    for i in final:
+        file.write(str(i) + ' ')
+    file.close()
